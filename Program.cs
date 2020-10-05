a1ab57293aeaab139d79c627acfe4732df5c33a7
Scott Vance <logic@newbaselogic.com>
2020-10-05 15:14:34 -0400

    - init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FigmaExtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FigmaExtraction");
+        }
+    }
+}
